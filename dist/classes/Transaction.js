2e59c3dfe3e7a426936e9cc35172db2e27dae9c2
Bryan Feiten <bryan.felipe.feiten@gmail.com>
2022-02-22 23:21:40 -0300

    again bug fix Transaction class

--- dist/classes/Transaction.js
+++ dist/classes/Transaction.js
@@ -1,11 +1,7 @@
-function generateRandomNumber(userId) {
-    let randomNumber = userId + Math.floor(Math.random() * 99999999999999999) + 1;
-    return randomNumber;
-}
-
 module.exports = class Transaction {
+
+    id = this.userId + Math.floor(Math.random() * 99999999999999999) + 1;
     constructor(userId, title, value, type) {
-        this.id = generateRandomNumber(this.userId);
         this.userId = userId;
         this.title = title;
         this.value = value;
