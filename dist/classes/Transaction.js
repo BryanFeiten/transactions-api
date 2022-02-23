f5368b1d69ae4d9e3ded1670afa211a63cee3ef4
Bryan Feiten <bryan.felipe.feiten@gmail.com>
2022-02-22 23:34:10 -0300

    test with es2016 target

--- dist/classes/Transaction.js
+++ dist/classes/Transaction.js
@@ -1,9 +1,16 @@
-module.exports = class Transaction {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function generateRandomNumber(userId) {
+    let randomNumber = userId + Math.floor(Math.random() * 99999999999999999) + 1;
+    return randomNumber;
+}
+class Transaction {
     constructor(userId, title, value, type) {
-        this.id = userId + Math.floor(Math.random() * 99999999999999999) + 1;
         this.userId = userId;
         this.title = title;
         this.value = value;
-        this.type = type
+        this.type = type;
+        this.id = generateRandomNumber(this.userId);
     }
 }
+exports.default = Transaction;
--- dist/classes/User.js
+++ dist/classes/User.js
@@ -1,16 +1,19 @@
-module.exports = class User {
-    id = (Math.floor(Math.random() * 99999999999999999) + 1).toString();
-    transactions = [];
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class User {
+    constructor(name, age, cpf, email) {
+        this.name = name;
+        this.age = age;
+        this.cpf = cpf;
+        this.email = email;
+        this.id = (Math.floor(Math.random() * 99999999999999999) + 1).toString();
+        this.transactions = [];
+    }
     get getTransactions() {
         return this.transactions;
     }
     setTransaction(newTransaction) {
         this.transactions.push(newTransaction);
     }
-    constructor(name, age, cpf, email) {
-        this.name = name;
-        this.age = age;
-        this.cpf = cpf;
-        this.email = email
-    }
 }
+exports.default = User;
